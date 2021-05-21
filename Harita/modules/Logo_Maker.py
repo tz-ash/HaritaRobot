40a7220e8c0cd4dba166f18327e5e965ccd945b9
Kid_of_devil <82517468+tz-ash@users.noreply.github.com>
2021-05-21 08:35:30 +0530

    Update Logo_Maker.py

--- Harita/modules/Logo_Maker.py
+++ Harita/modules/Logo_Maker.py
@@ -39,11 +39,11 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=15, stroke_fill="yellow")
     fname2 = "LogoByAnie.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By Harita")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By Axia")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @HaritaSupport, {e}')
+   await event.reply(f'Error Report @magiclmush, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
